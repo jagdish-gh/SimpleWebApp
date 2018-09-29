c745d3bc4a2d81af4d7417f524039b1808916412
Jagdish Prasad Choudhary <jagdishiitp@gmail.com>
2018-09-29 12:43:38 +0530

    Adding port

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.put('/image',(req, res) =>{image.handleimage(req, res, db )})
 app.post('/imageurl',(req, res) =>{image.handleApicall(req, res )})
 
 app.listen(process.env.PORT || 3000, ()=>{
-	console.log('app is running on port ${process.env.PORT}')
+	console.log(`app is running on port ${process.env.PORT}`)
 })
